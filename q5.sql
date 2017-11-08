8e19a089d52f4be93b258644518861dfc308b490
Syed Ali <sd.ali@mail.utornto.ca>
2017-11-08 06:05:04 -0500

    finsihed q5

--- q5.sql
+++ q5.sql
@@ -12,12 +12,75 @@ CREATE TABLE q5(
         stateMarket REAL
 );
 
--- You may find it convenient to do this for each of the views
--- that define your intermediate steps.  (But give them better names!)
-DROP VIEW IF EXISTS intermediate_step CASCADE;
-
--- Define views for your intermediate steps here.
 
 
--- the answer to the query 
-insert into q5 
+CREATE VIEW recentCabinet AS
+SELECT id, country_id
+FROM cabinet
+WHERE EXTRACT(YEAR FROM start_date) >= 1997
+ORDER BY country_id;
+
+
+
+CREATE VIEW countryCabAct AS
+SELECT country_id, party_id, cabinet_id
+FROM recentCabinet JOIN cabinet_party ON recentCabinet.id = cabinet_id
+ORDER BY country_id, party_id, cabinet_id;
+
+
+
+
+CREATE VIEW countryCabShould AS
+SELECT party.country_id, party.id AS party_id, cabinet.id AS cabinet_id
+FROM party JOIN cabinet ON party.country_id = cabinet.country_id
+ORDER BY country_id, party_id, cabinet_id;
+
+
+
+
+CREATE VIEW notcommited AS
+(SELECT * FROM countryCabShould) EXCEPT ALL (SELECT * FROM countryCabAct);
+
+
+
+CREATE VIEW notcommitedParty AS
+SELECT DISTINCT party_id AS id
+FROM notcommited
+ORDER BY party_id;
+
+
+
+CREATE VIEW commitedparty AS
+(SELECT id FROM party) EXCEPT (SELECT id FROM notcommitedParty);
+
+
+
+CREATE VIEW binder AS
+SELECT country_id, party.id AS party_id, party.name AS partyName
+FROM party JOIN commitedparty ON party.id = commitedparty.id
+ORDER BY party.id;
+
+
+
+CREATE VIEW binder2 AS
+SELECT country_id, binder.party_id, partyName, family
+FROM binder LEFT JOIN party_family ON party_family.party_id = binder.party_id
+ORDER BY binder.party_id;
+
+
+CREATE VIEW binder3 AS
+SELECT country_id, binder2.party_id, partyName, family, state_market
+FROM binder2 LEFT JOIN party_position ON party_position.party_id = binder2.party_id
+ORDER BY binder2.party_id;
+
+
+CREATE VIEW binder4 AS
+SELECT country.name AS countryName, partyName, family, state_market
+FROM binder3 JOIN country ON country_id = country.id
+ORDER BY country.name;
+
+
+
+INSERT INTO q5
+SELECT countryName, partyName, family, state_market
+FROM binder4;
---
+++ q5_order.sql
@@ -0,0 +1,6 @@
+SET SEARCH_PATH TO parlgov;
+
+
+SELECT * 
+FROM q5
+ORDER BY countryName, partyName, stateMarket DESC;
