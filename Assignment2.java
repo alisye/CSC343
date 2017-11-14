81dcdcf679e8f4061efc4e47026d159d7fc068c0
Syed Ali <sd.ali@mail.utornto.ca>
2017-11-14 01:06:34 -0500

    finished A2 Q3

--- Assignment2.java
+++ Assignment2.java
@@ -4,9 +4,9 @@ import java.util.List;
 // If you are looking for Java data structures, these are highly useful.
 // Remember that an important part of your mark is for doing as much in SQL (not Java) as you can.
 // Solutions that use only or mostly Java will not receive a high mark.
-//import java.util.ArrayList;
-//import java.util.Map;
-//import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 //import java.util.Set;
 //import java.util.HashSet;
 public class Assignment2 extends JDBCSubmission {
@@ -43,27 +43,113 @@ public class Assignment2 extends JDBCSubmission {
 
     @Override
     public ElectionCabinetResult electionSequence(String countryName) {
-        // Implement this method!
 	ElectionCabinetResult result = new ElectionCabinetResult(new ArrayList<Integer> (), new ArrayList<Integer> ());
-	
+	try {	
+		//get countryid first
+		String countryIDquery = "SELECT id FROM country WHERE name = ?";
+		PreparedStatement ps = connection.prepareStatement(countryIDquery);
+		ps.setString(1, countryName);
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		int countryId = rs.getInt("id");
 
 
+		String elections = "SELECT id, e_date FROM election WHERE country_id = ? ORDER BY EXTRACT(YEAR FROM e_date) DESC";
+		PreparedStatement ps1 = connection.prepareStatement(elections);
+		ps1.setInt(1, countryId);
+		ResultSet rs1 = ps1.executeQuery();
+		
+		HashMap<Integer, String> electiondates = new HashMap<Integer, String>();
+		while(rs1.next()) {
+			int nextele = rs1.getInt("id");
+			result.elections.add(nextele);
+
+			String date = rs1.getString("e_date");
+			electiondates.put(nextele, date);
+		}
+
+		for (int electionIds : result.elections) {
+			String typequery = "SELECT e_type, previous_parliament_election_id, previous_ep_election_id FROM election WHERE id = " + electionIds;
+			PreparedStatement ps2 = connection.prepareStatement(typequery);
+			ResultSet typetable = ps2.executeQuery();
+			typetable.next();
+			String type = typetable.getString("e_type");
+			
+			String next_type;
+			if (type.equals("European Parliament")) {	
+				next_type = "previous_ep_election_id";
+			} else {
+				next_type = "previous_parliament_election_id";
+			}
+		
+
+			String findnext = "SELECT id FROM election WHERE " + next_type + " = " + Integer.toString(electionIds);
+			PreparedStatement ps3 = connection.prepareStatement(findnext);
+			ResultSet nextEL = ps3.executeQuery();
+		
+			boolean next_exists = nextEL.next();
+			String dateQuery = "SELECT e_date FROM election WHERE id = " + Integer.toString(electionIds);
+			PreparedStatement preparedate = connection.prepareStatement(dateQuery);
+			ResultSet dateres = preparedate.executeQuery();
+			dateres.next();
+			Date current_electionDate = dateres.getDate("e_date");
+			Date next_electionDate;
+				
+			if (next_exists) {
+				int nextelection = nextEL.getInt("id");
+				String nextDateQ = "SELECT e_date FROM election WHERE id = " + Integer.toString(nextelection);
+				PreparedStatement preparenextdate = connection.prepareStatement(nextDateQ);
+				ResultSet nextres = preparenextdate.executeQuery();
+				nextres.next();
+				next_electionDate = nextres.getDate("e_date");
+				String findCabs = "SELECT id FROM cabinet WHERE start_date > ? and start_date < ?";
+				PreparedStatement ps4 = connection.prepareStatement(findCabs);	
+				ps4.setDate(1, current_electionDate);
+				ps4.setDate(2, next_electionDate);
+				ResultSet validcabs1 = ps4.executeQuery();
+				while (validcabs1.next()){
+					int cabs = validcabs1.getInt("id");
+					result.cabinets.add(cabs);
+				}
+
+			} else {
+				String findfirstCabs = "SELECT id FROM cabinet WHERE start_date > ?";
+				PreparedStatement ps5 = connection.prepareStatement(findfirstCabs);
+				ps5.setDate(1, current_electionDate);
+				ResultSet validcabs2 = ps5.executeQuery();
+				while (validcabs2.next()) {
+					int cabs = validcabs2.getInt("id");
+					result.cabinets.add(cabs);
+				}
+				
+			}
+		}
+		
+		System.out.println(result.toString());
+		return result;
+	}
+	catch (SQLException se)
+	{
+		System.err.println("SQL EXCEPTION: <MESSAGE:> " + se.getMessage());
+		return null;
+	}
     }
 
     @Override
     public List<Integer> findSimilarPoliticians(Integer politicianName, Float threshold) {
-        // Implement this method!
-        return null;
+        
+	return null;
     }
 
     public static void main(String[] args) {
         // You can put testing code in here. It will not affect our autotester.
     	try {
 	    Assignment2 test = new Assignment2();
-	    boolean t = test.connectDB("jdbc:postgresql://localhost:5432/csc343h-alisye55", "alisye55", "");
+	    boolean t = test.connectDB("jdbc:postgresql://localhost:5432/csc343h-alisye55?currentSchema=parlgov", "alisye55", "");
 	    System.out.println(t);
+	    test.electionSequence("Germany");
 	    boolean t1 = test.disconnectDB();
-	    System.out.println(t);
+	    System.out.println(t1);
 	}
 	catch (ClassNotFoundException e) {
 	    System.out.println("Failed to find JDBC driver");
